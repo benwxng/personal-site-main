a814fc5208861738c170f5c3c09ede15adf0c190
benwxng <benxwang7@gmail.com>
2024-07-22 23:42:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heading = document.getElementById('heading');
+    const description = document.getElementById('description');
+    const currently = document.querySelector('.currently');
+
+    setTimeout(() => {
+        heading.style.opacity = 1;
+        heading.style.transform = "translateX(0)";
+    }, 400);
+
+    setTimeout(() => {
+        description.style.opacity = 1;
+        description.style.transform = "translateX(0)";
+    }, 500);
+
+    setTimeout(() => {
+        currently.style.opacity = 1;
+        currently.style.transform = "translateY(0)";
+    }, 1000)
+});
