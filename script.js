ec07eb6371ed1b575f1ea8c6a634967f0d06de7d
benwxng <benxwang7@gmail.com>
2024-07-25 12:41:02 -0400

    update :)

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const heading = document.getElementById('heading');
     const description = document.getElementById('description');
     const currently = document.querySelector('.currently');
+    const links = document.querySelector('.links');
+    const substack = document.getElementById('substack');
+    const bookshelf = document.getElementById('bookshelf');
+    const instagram = document.getElementById('instagram');
+    
+ 
 
     setTimeout(() => {
         heading.style.opacity = 1;
@@ -11,10 +17,29 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(() => {
         description.style.opacity = 1;
         description.style.transform = "translateX(0)";
-    }, 500);
+    }, 600);
 
     setTimeout(() => {
         currently.style.opacity = 1;
         currently.style.transform = "translateY(0)";
     }, 1600)
+
+    setTimeout(() => {
+
+       substack.style.opacity = 1;
+       
+    }, 2200)
+
+    setTimeout(() => {
+
+        bookshelf.style.opacity = 1;
+        
+     }, 2400)
+
+     setTimeout(() => {
+
+        instagram.style.opacity = 1;
+        
+     }, 2600)
+    
 });
